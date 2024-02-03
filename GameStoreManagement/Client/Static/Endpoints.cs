5f95a8a69de671e146f2d37dd269d6af64300449
MUDIYANSELAGE HIRAN ERANDA WIJERATNE 2101015B <2101015B@student.tp.edu.sg>
2024-02-03 23:39:57 +0800

    Before Validation

---
+++ GameStoreManagement/Client/Static/Endpoints.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStoreManagement.Client.Static
+{
+    public class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        // Use consistent naming for the Prefix field
+        public static readonly string GamesEndpoint = $"{Prefix}/games";
+        public static readonly string ReviewsEndpoint = $"{Prefix}/reviews";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+    }
+}
